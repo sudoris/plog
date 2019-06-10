5f37865d53bdc143ef97bb95e7a5bcc9da38cbfb
dchen21 <zdent09@gmail.com>
2019-06-10 20:34:16 +0800

    blah

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,69 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+// show about section
+function showAbout() {  
+  let el = document.getElementById('about-container')
+  el.setAttribute('style', 'display: block;')
+  el.classList.add('animated', 'slideInLeft', 'faster')
+  
+  setTimeout(function(){
+      el.classList.remove('animated', 'slideInLeft', 'faster')        
+  }, 800);
+}
+
+// close about section
+function closeAbout() {    
+  let el = document.getElementById('about-container')
+  el.classList.add('animated', 'slideOutLeft', 'faster')
+  
+  setTimeout(function(){
+      el.classList.remove('animated', 'slideOutLeft', 'faster')
+      el.setAttribute('style', 'display: none;')        
+  }, 800);
+}
+
+// show contact section
+function showContact(){
+  let el = document.getElementById('contact-container')
+  el.setAttribute('style', 'display: block;')
+  el.classList.add('animated', 'slideInUp', 'faster')
+  
+  setTimeout(function(){
+      el.classList.remove('animated', 'slideInUp', 'faster')        
+  }, 800);
+}
+
+// close contact section
+function closeContact(){
+  let el = document.getElementById('contact-container')
+  el.classList.add('animated', 'slideOutDown', 'faster')
+  
+  setTimeout(function(){
+      el.classList.remove('animated', 'slideOutDown', 'faster')
+      el.setAttribute('style', 'display: none;')     
+  }, 800);
+}
+
+// show work section
+function showWork(){
+  let el = document.getElementById('work-container')
+  el.setAttribute('style', 'display: block;')
+  el.classList.add('animated', 'slideInRight', 'faster')
+  
+  setTimeout(function(){
+      el.classList.remove('animated', 'slideInRight', 'faster')    
+  }, 800);
+}
+
+// close work section
+function closeWork(){
+  let el = document.getElementById('work-container')
+  el.classList.add('animated', 'slideOutRight', 'faster')
+  
+  setTimeout(function(){
+      el.classList.remove('animated', 'slideOutRight', 'faster')
+      el.setAttribute('style', 'display: none;')           
+  }, 800);
+}
